b6d16d0b991e9ad5f71aa79dc404c6e748944a94
aeromouse007 <aeromouse007@gmail.com>
2022-10-06 23:09:24 +0800

    add a new

---
+++ password_retry.py
@@ -0,0 +1,13 @@
+password = 'a123456' 
+i = 3 #剩下機會
+while True: 
+	pwd = input ('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功!!')
+		break #跳出迴圈
+	else: 
+		i = i - 1
+		print('登入錯誤,還有', i,'次機會')
+		if i == 0:
+			break
+	
